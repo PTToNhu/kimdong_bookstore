e370ef73d5f78a8a5ee84925bc14b310631313ed
ThuwTrinh <trinhthianhthu1808@gmail.com>
2025-03-04 12:10:47 +0700

    Not full

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from '../components/Home'
 import Category from '../components/Category'
+import AboutUs from '../components/AboutUs'
+import Master_Classe from '../components/Master_Classe'
+import FAQ from '../components/FAQ'
+import Question from '../components/question'
 
 function App() {
 
@@ -9,6 +13,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/about_us" element={<AboutUs />} />
+        <Route path="/about_us/master_classe" element={<Master_Classe />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/faq/question" element={<Question />} />
         <Route path="/category" element={<Category />} />
       </Routes>
     </BrowserRouter>
